bc721cb2d1f5fa9ca71c85e01f23a3854282878f
lipan <lipan.me@qq.com>
2022-03-22 17:37:20 +0800

    up

--- src/main/java/com/zaqbest/study/foundation/alg/zcy/top_interview/topinterviewquestions/Problem_0045_JumpGameII.java
+++ src/main/java/com/zaqbest/study/foundation/alg/zcy/top_interview/topinterviewquestions/Problem_0045_JumpGameII.java
@@ -1,14 +1,13 @@
 package com.zaqbest.study.foundation.alg.zcy.top_interview.topinterviewquestions;
 
 public class Problem_0045_JumpGameII {
-
 	public static int jump(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
 		int step = 0;
-		int cur = 0;
-		int next = arr[0];
+		int cur = 0; //再走step的时候，最远可以走到的位置
+		int next = arr[0]; //如果再走一步，能到达的最远位置
 		for (int i = 1; i < arr.length; i++) {
 //            if(next >= arr.length - 1){
 //                return step + 1;
@@ -21,5 +20,4 @@ public class Problem_0045_JumpGameII {
 		}
 		return step;
 	}
-
 }
